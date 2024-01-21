1d20de57cacb1780d9c95ac539ef8fae530bb2c0
berenelcn <polatberen24@gmail.com>
2024-01-22 00:46:39 +0300

    Turret Bullet Spawn
    Turrets can spawn bullets in an interval in the direction of enemy. But cant shoot enemy(idk why). Also made them isTrigger(enemy and bullet)

---
+++ Assets/Scripts/TurretBulletSpawn/TurretBulletSpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class TurretBulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public GameObject enemyPrefab;
+    public float bulletSpeed = 10f;
+    public float spawnInterval = 10f;
+    public float bulletLifetime = 5f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnBullet", 0f, spawnInterval);
+    }
+
+    void SpawnBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+        Vector3 direction = (enemyPrefab.transform.position - transform.position).normalized;
+
+        Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+        bulletRb.velocity = direction * bulletSpeed;
+
+        Destroy(bullet, bulletLifetime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
