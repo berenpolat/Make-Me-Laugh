a6aa59a4d3c3300467767f78769a3988784131c3
Melisa Ronay Seven <melisar.seven@gmail.com>
2024-01-27 02:54:22 +0300

    Add enemy damage system

--- Assets/Scripts/EnemyScripts/EnemyAI.cs
+++ Assets/Scripts/EnemyScripts/EnemyAI.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace EnemyScripts
@@ -9,22 +10,51 @@ namespace EnemyScripts
         MeleeBoss = 2,
         RangedBoss = 3,
     }
+
+    [System.Serializable]
+    public struct MoneyAndPointInformation
+    {
+        public float givenPointsToThePlayer;
+        public float givenMoneyToThePlayer;
+    }
     public class EnemyAI : MonoBehaviour
     {
         [SerializeField] private EnemyType enemyType;
         [SerializeField] private float damageAmount;
         [SerializeField] private float maxHp;
-        [SerializeField] private float givenPointsToThePlayer;
-        [SerializeField] private float givenMoneyToThePlayer;
+        [SerializeField] private MoneyAndPointInformation moneyAndPointInformation;
         
         //TODO: Add animation control for attack status
 
         private float currentHp;
 
-        public float CurrentHp
+        //TODO: For enemy hp bar
+        public float CurrentHp => currentHp;
+
+        private void Start()
         {
-            get => currentHp;
-            set => currentHp = value;
+            currentHp = maxHp;
+        }
+
+        public void LowerHealth(float decreaseAmount)
+        {
+            currentHp -= decreaseAmount;
+        }
+
+        public bool CheckIsDead()
+        {
+            return currentHp <= 0;
+        }
+
+        public MoneyAndPointInformation GetMoneyAndPointInformation()
+        {
+            return moneyAndPointInformation;
+        }
+
+        public void DestroyEnemy()
+        {
+            //TODO: Also spawn body prefab here
+            Destroy(gameObject);
         }
     }
 }
